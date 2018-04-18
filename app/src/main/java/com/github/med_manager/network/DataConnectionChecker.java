3cf2470c4be08f68d44b87d48877a1fb053f529c
Ese Udom <udomese@yahoo.com>
2018-04-18 16:27:41 +0100

    Add files via upload

--- app/src/main/java/com/github/med_manager/network/DataConnectionChecker.java
+++ app/src/main/java/com/github/med_manager/network/DataConnectionChecker.java
@@ -1,1 +1,43 @@
+/*
+ * Copyright 2018 Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.med_manager.network;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by Ese Udom on 1/3/2017.
+ */
+
+/** Manages data connection */
+public class DataConnectionChecker {
+    private Context _context;
+
+    public DataConnectionChecker(Context context) {
+        this._context = context;
+    }
+
+    /** Checks for network connection */
+    public boolean isConnected() {
+        ConnectivityManager cm =
+                (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return (activeNetwork != null && activeNetwork.isConnectedOrConnecting());
+    }
+
+}
